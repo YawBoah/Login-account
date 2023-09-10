efd5915c603d951475f0a3d3da6e7cc53d450383
YBoah <yawboahene.yb@gmail.com>
2023-09-10 10:39:21 +0000

    More changes

--- login/index.php
+++ login/index.php
@@ -1,14 +1,9 @@
 <?php
-
-$error = array();
-if (empty($fullname) OR empty($email) OR empty($password)){
-  array_push($errors,"All fields are required"); 
-}
-if (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
-  array.push($errors, )
+session_start();
+if (isset($_SESSION["user"])) {
+   header("Location: index.php");
 }
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -27,6 +22,29 @@ if (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
   </head>
   <body>
     <div class="content">
+    <?php
+        if (isset($_POST["login"])) {
+           $email = $_POST["email"];
+           $password = $_POST["password"];
+            require_once "../database.php";
+            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            if ($user) {
+                if (password_verify($password, $user["password"])) {
+                    session_start();
+                    $_SESSION["user"] = "yes";
+                    header("Location: index.php");
+                    die();
+                }else{
+                    echo "<div class='alert alert-danger'>Password does not match</div>";
+                }
+            }else{
+                echo "<div class='alert alert-danger'>Email does not match</div>";
+            }
+        }
+        ?>
+
       <div class="left-content">
         <div class="main">
           <h1>GoFinance</h1>
@@ -54,7 +72,7 @@ if (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
         </div>
         <br />
 
-        <button>Login</button>
+        <button name="submit">Login</button>
         </form>
         <br />
         <br />
